aebd53ac6c816f95b702ce4a4c32fd10de34a3e1
Andrew Evripidou <aevripidou1996@gmail.com>
2023-07-12 19:38:09 +1000

    fixed local host

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 // Load the Mongoose ORM
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
